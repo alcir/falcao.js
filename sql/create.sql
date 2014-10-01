19e5716cb3fb69e7be13aecc27e5f31ae858c120
alcir <alciregi@gmail.com>
2014-10-01 16:05:36 +0200

    The IP record must be CHAR(15)

--- sql/create.sql
+++ sql/create.sql
@@ -1,10 +1,10 @@
 CREATE TABLE `probes` (
   `mac` char(17) NOT NULL DEFAULT '',
-  `ip` char(12) NOT NULL,
+  `ip` char(15) NOT NULL,
   `hostname` char(253) NOT NULL DEFAULT '',
   `firstseen` int(11) DEFAULT NULL,
   `lastseen` int(11) DEFAULT NULL,
   `lastcheck` int(11) DEFAULT NULL,
+  `agent` char(10) DEFAULT NULL,
   PRIMARY KEY (`mac`,`ip`,`hostname`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
